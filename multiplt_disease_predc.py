278f60d679b4ff812d1ea12c541d243d3387c3bf
Nishita Jain <75923914+NiShITa-code@users.noreply.github.com>
2023-09-03 17:49:24 +0530

    Update multiplt_disease_predc.py

--- multiplt_disease_predc.py
+++ multiplt_disease_predc.py
@@ -4,60 +4,89 @@ Created on Sat Apr  8 14:20:12 2023
 
 @author: HP
 """
-
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
+
+
 # loading the saved models
+
 diabetes_model = pickle.load(open('saved models/diabetes_model.sav', 'rb'))
 
 heart_disease_model = pickle.load(open('saved models/heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('saved models/parkinsons_model.sav', 'rb'))
-#sidebars for navigation
+parkinsons_model = pickle.load(open('saved parkinsons_model.sav', 'rb'))
+
+
+
+# sidebar for navigation
 with st.sidebar:
-    selected = option_menu('MULTIPLE DISEASE PREDICTION SYSTEM', 
-                           
-                           ['Diabetes prediction', 'Heart Disease Prediction', 'Parkinsons Prediction'], 
-                           icons= ['file-medical-fill', 'heart', 'file-person'],
-                           default_index= 0)
     
-# diabetes prediction page
-if (selected == 'Diabetes prediction'):
-    #page title
-    st.title('Diabetes prediction using ML')
-    #getting the input data from the user
+    selected = option_menu('Multiple Disease Prediction System',
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
+    
+    # page title
+    st.title('Diabetes Prediction using ML')
+    
+    
+    # getting the input data from the user
     col1, col2, col3 = st.columns(3)
+    
     with col1:
         Pregnancies = st.text_input('Number of Pregnancies')
-    with col2:    
-        Glucose = st.text_input('Glucose level')
+        
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    
     with col3:
         BloodPressure = st.text_input('Blood Pressure value')
+    
     with col1:
         SkinThickness = st.text_input('Skin Thickness value')
+    
     with col2:
-        Insulin = st.text_input('Insulin level')
-    with col3:    
-        BMI = st.text_input('BMI Value')
+        Insulin = st.text_input('Insulin Level')
+    
+    with col3:
+        BMI = st.text_input('BMI value')
+    
     with col1:
         DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    with col2:
-        Age = st.text_input('Age of the person')
     
-    #code for prediction
+    with col2:
+        Age = st.text_input('Age of the Person')
+    
+    
+    # code for Prediction
     diab_diagnosis = ''
     
-    #button for reslut
+    # creating a button for Prediction
+    
     if st.button('Diabetes Test Result'):
         diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        
         if (diab_prediction[0] == 1):
-            diab_diagnosis = 'The person is Diabetic'
-        else :
-            diab_diagnosis = 'The person is not Diabetic'
-    st.success(diab_diagnosis)  
-# heart disease prdiction page       
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
+
+
+
+
+# Heart Disease Prediction Page
 if (selected == 'Heart Disease Prediction'):
+    
     # page title
     st.title('Heart Disease Prediction using ML')
     
@@ -120,8 +149,11 @@ if (selected == 'Heart Disease Prediction'):
         
     st.success(heart_diagnosis)
         
+    
+    
+
 # Parkinson's Prediction Page
-if (selected == 'Parkinsons Prediction'):
+if (selected == "Parkinsons Prediction"):
     
     # page title
     st.title("Parkinson's Disease Prediction using ML")
