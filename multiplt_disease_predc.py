37fba9649089fc6ca5407c5a26d96ec97d42fcd9
Nishita Jain <75923914+NiShITa-code@users.noreply.github.com>
2023-09-03 17:35:49 +0530

    Update multiplt_disease_predc.py

--- multiplt_disease_predc.py
+++ multiplt_disease_predc.py
@@ -9,11 +9,11 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 # loading the saved models
-diabetes_model = pickle.load(open('saved models\diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('saved models\heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('saved models/heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('saved models\parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('saved models/parkinsons_model.sav', 'rb'))
 #sidebars for navigation
 with st.sidebar:
     selected = option_menu('MULTIPLE DISEASE PREDICTION SYSTEM', 
